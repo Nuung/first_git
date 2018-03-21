2dc53194f7026ac58a4c8100066165f24b6b68db
Nuung <qlgks1@naver.com>
2018-03-21 11:28:44 +0900

    create "hello my first git project!"

---
+++ bool.rb
@@ -0,0 +1,16 @@
+
+puts("What ur name")
+in_name = gets.chomp();
+puts("Put ur password!")
+in_str = gets.chomp();
+
+if in_str=="1234"
+	puts("Welcome! : "+in_name)
+	puts("what u press password : "+in_str)
+elsif in_str=="11"
+	puts("Welcome! : "+in_name)
+	puts("what u press password : "+in_str)
+else
+	puts("what the fuxk, who u r?")
+	puts("I dont know ["+in_name+"] is who are!")
+end
