1bfdaeeac8bdbd4d6b0df0316332e83c918b0343
Nuung <qlgks1@naver.com>
2018-03-21 11:25:51 +0900

    .bool.py.swp

---
+++ bool.py
@@ -0,0 +1,13 @@
+
+in_name = input("What ur name")
+in_str = input("Put ur password!")
+
+if in_str=="1234":
+	print("Welcome! : "+in_name)
+	print("what u press password : "+in_str)
+elif in_str=="11":
+	print("Welcome! : "+in_name)
+	print("what u press password : "+in_str)
+else:
+	print("what the fuxk, who u r?")
+	print("I dont know ["+in_name+"] is who are!")
